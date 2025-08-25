7d15bb04a52e3b81a9967d5dfc0bb4a112e7ebb5
mmk1484 <motyomaru@yahoo.co.jp>
2025-08-26 04:29:26 +0900

    kadai_021を修正

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,8 +1,8 @@
 const btn = document.getElementById("btn");
-const tezt = document.getElementById("text");
+const text = document.getElementById("text");
 
 btn.addEventListener("click", () => {
   setTimeout(() => {
-    document.getElementById("text").textContent = "ボタンをクリックしました";
+    text.textContent = "ボタンをクリックしました";
   }, 2000);
 });
